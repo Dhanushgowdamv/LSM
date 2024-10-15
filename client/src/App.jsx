3efc2fb69e0921625552a29f038d7a186e1e7e50
Dhanush Gowda mv <dhanushvenu199@gmail.com>
2024-10-16 04:14:53 +0530

    ground set up

---
+++ client/src/App.jsx
@@ -0,0 +1,14 @@
+
+import { Button } from './components/ui/button'
+
+function App() {
+  
+
+  return (
+    <>
+    <Button className=" bg-red-700">check</Button>
+    </>
+  )
+}
+
+export default App
